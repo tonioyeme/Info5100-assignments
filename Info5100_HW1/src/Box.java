38ef0184f1ada6b330d0bdff6b1ba1cc8b1f10b0
tonioyeme <36220865+tonioyeme@users.noreply.github.com>
2022-02-08 23:22:21 -0800

    Delete Info5100_HW1 directory

--- Info5100_HW1/src/Box.java
+++
@@ -1,9 +0,0 @@
-public class Box {
-    public double calculateBoxVolume;
-    double width;
-    double height;
-    double depth;
-
-    public Box(double width, double height, double depth) {
-    }
-}
