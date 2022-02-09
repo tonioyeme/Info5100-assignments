92e8caba33d254c08dfee0ac1548223bd96232e9
tonioyeme <36220865+tonioyeme@users.noreply.github.com>
2022-02-08 22:32:44 -0800

    Add files via upload

---
+++ Info5100_HW1/src/Box.java
@@ -0,0 +1,9 @@
+public class Box {
+    public double calculateBoxVolume;
+    double width;
+    double height;
+    double depth;
+
+    public Box(double width, double height, double depth) {
+    }
+}
